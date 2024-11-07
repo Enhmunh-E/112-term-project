7451f274b8bffae96057fa62a1ecf451b41c6d83
Enhmunh-E <enhuush0704@gmail.com>
2024-11-07 23:18:24 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 112-term-project
+
+## To use the project
+
+First, download cmu graphics and move it to the project directory from the following link https://academy.cs.cmu.edu/desktop
+Then use app.py to use the project
---
+++ app.py
@@ -0,0 +1,35 @@
+#################################################
+# project.py
+#
+# Your name: Enkhmunkh Enkhbayar
+# Your andrew id: eenkhbay
+#################################################
+
+
+from cmu_graphics import *
+import random
+import copy
+
+
+def onAppStart(app):
+    app.RES = app.WIDTH, app.HEIGHT = 1280, 720
+    pass
+
+
+def redrawAll(app):
+    pass
+
+
+def onKeyPress(app, key):
+    pass
+
+
+def onStep(app):
+    pass
+
+
+def main():
+    runApp(1280, 720)
+
+
+main()
