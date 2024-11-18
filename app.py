927e624490aa86fc2f966030436f5aa8a39c9797
Enhmunh-E <enhuush0704@gmail.com>
2024-11-19 00:28:24 +0300

    camera orientation with cursor

--- app.py
+++ app.py
@@ -7,14 +7,7 @@
 
 
 from cmu_graphics import *
-import random
-import copy
-import numpy as np
 import math
-
-
-def flatten(xss):
-    return [x for xs in xss for x in xs]
 
 
 class Object:
@@ -36,9 +29,14 @@ class Object:
         self.position = (self.position[0], self.position[1], z)
 
     def changeOrientation(self, x, y, z):
+        newY = self.orientation[1] + y
+        if newY > math.pi:
+            newY = math.pi
+        elif newY < 0:
+            newY = 0
         self.orientation = (
             self.orientation[0] + x,
-            self.orientation[1] + y,
+            newY,
             self.orientation[2] + z,
         )
 
@@ -140,6 +138,7 @@ MAXRES = max(WIDTH, HEIGHT)
 def onAppStart(app):
     app.RES = app.WIDTH, app.HEIGHT = 800, 800
     app.camera = Camera()
+    app.dragStartingPosition = None
     app.world = World()
     app.world.createBlock((0, 0, 0))
     app.world.createBlock((0, 1, 0))
@@ -152,39 +151,24 @@ def getPositionOnScreen(app, pos):
     x, y, z = pos
     COx, COy, COz = app.camera.orientation
     Cx, Cy, Cz = app.camera.position
-    a = (
-        np.matrix(
-            [
-                [1, 0, 0],
-                [0, math.cos(COx), math.sin(COx)],
-                [0, -math.sin(COx), math.cos(COx)],
-            ]
-        )
-        * np.matrix(
-            [
-                [math.cos(COy), 0, -math.sin(COy)],
-                [0, 1, 0],
-                [math.sin(COy), 0, math.cos(COy)],
-            ]
-        )
-        * np.matrix(
-            [
-                [math.cos(COz), math.sin(COz), 0],
-                [-math.sin(COz), math.cos(COz), 0],
-                [0, 0, 1],
-            ]
-        )
-        * (np.matrix([[x], [y], [z]]) - np.matrix([[Cx], [Cy], [Cz]]))
-    )
-
-    a = a.flatten().getA()[0]
-    x, y, z = a
-    if z == 0:
+    difX = x - Cx
+    difY = y - Cy
+    difZ = z - Cz
+    Sx = math.sin(COx)
+    Sy = math.sin(COy)
+    Sz = math.sin(COz)
+    Cx = math.cos(COx)
+    Cy = math.cos(COy)
+    Cz = math.cos(COz)
+    X = Cy * (Sz * difY + Cz * difX) - Sy * difZ
+    Y = Sx * (Cy * difZ + Sy * (Sz * difY + Cz * difX)) + Cx * (Cz * difY - Sz * difX)
+    Z = Cx * (Cy * difZ + Sy * (Sz * difY + Cz * difX)) - Sx * (Cz * difY - Sz * difX)
+    if Z == 0:
         py = 0
         px = 0
     else:
-        py = y / -z
-        px = x / -z
+        py = Y / -Z
+        px = X / -Z
     py_map = (1 + py) / 2
     px_map = (1 + px) / 2
     return int(py_map * MAXRES), int(px_map * MAXRES)
@@ -224,6 +208,24 @@ def onKeyHold(app, keys):
     pass
 
 
+def onMouseDrag(app, mouseX, mouseY, button):
+    # if button == 2:
+    # Right Click
+    if app.dragStartingPosition:
+        # Meaning it has already started dragging
+        disX = app.dragStartingPosition[0] - mouseX
+        disY = app.dragStartingPosition[1] - mouseY
+        app.camera.changeOrientation(
+            0, math.pi / 180 * disY / 100, math.pi / 180 * disX / 100
+        )
+    else:
+        app.dragStartingPosition = (mouseX, mouseY)
+
+
+def onMouseRelease(app, mouseX, mouseY, button):
+    app.dragStartingPosition = None
+
+
 def onStep(app):
     pass
 
@@ -233,3 +235,34 @@ def main():
 
 
 main()
+
+
+# def flatten(xss):
+#     return [x for xs in xss for x in xs]
+# a = (
+#     np.matrix(
+#         [
+#             [1, 0, 0],
+#             [0, math.cos(COx), math.sin(COx)],
+#             [0, -math.sin(COx), math.cos(COx)],
+#         ]
+#     )
+#     * np.matrix(
+#         [
+#             [math.cos(COy), 0, -math.sin(COy)],
+#             [0, 1, 0],
+#             [math.sin(COy), 0, math.cos(COy)],
+#         ]
+#     )
+#     * np.matrix(
+#         [
+#             [math.cos(COz), math.sin(COz), 0],
+#             [-math.sin(COz), math.cos(COz), 0],
+#             [0, 0, 1],
+#         ]
+#     )
+#     * (np.matrix([[x], [y], [z]]) - np.matrix([[Cx], [Cy], [Cz]]))
+# )
+
+# a = a.flatten().getA()[0]
+# x, y, z = a
